e22486ceaade9f002cc883c4eb518876b2465174
Connors <Brendan.Connors@dfo-mpo.gc.ca>
2022-11-07 13:28:44 -0800

    revise er and prod index plots to only include CC salmon CUs

--- plots.R
+++ plots.R
@@ -26,9 +26,20 @@ theme_sleek <- function(base_size = 11, base_family = "") {
 # load data ----
 er_data_pse <- read.csv("data/CU-ERs-Nov_2_2022_11_11_22_QueryResults.csv") # from PSE
 resid_data_pse <- read.csv("data/CU-resids-Nov_4_2022_14_57_48_QueryResults.csv") # from PSE
+cu_metadata <- read.csv("./data/CU_Metadata_20220812.csv",  
+                        header=TRUE, na.strings=c("", "NA")) %>%
+  filter(!is.na(gen_length)) %>%
+  rename(location=cuname.x) %>%
+  group_by(location) %>%
+  summarize(region = unique(Region))
+
+
+er_data_pse_cc <- merge(er_data_pse,cu_metadata, by = "location") %>% filter(region == "CC")
+resid_data_pse_cc <- merge(resid_data_pse,cu_metadata, by = "location") %>% filter(region == "CC") 
+
 
 # er plots ----
-er_data <- er_data_pse %>%
+er_data <- er_data_pse_cc %>%
   group_by(species, year) %>%
   summarise(avg_er = mean(datavalue)) %>%
   select(species, year,avg_er)
@@ -41,9 +52,17 @@ ggplot(er_data %>% filter(year<2017), aes( y = avg_er*100, x = year)) +
 
 ggsave("figs/all-er-avg.jpeg", width = 8, height = 5, units = "in")
 
+ggplot(er_data_pse_cc %>% filter(year<2017), aes(fill=location, y = datavalue*100, x = year)) +
+  geom_line(size=0.5, color="grey") +
+  labs(x = "Year", y = "Harvest rate (%)") +
+  facet_wrap(~species) +
+  theme_sleek() 
+
+ggsave("figs/all-er.jpeg", width = 8, height = 5, units = "in")
+
 # resid plots ----
 
-data_resid <- resid_data_pse %>%
+data_resid <- resid_data_pse_cc %>%
   pivot_wider(names_from = parameter, values_from = datavalue) %>%
   drop_na(Recruits) %>%
   mutate(prod = log(Recruits/Spawners)) %>%
@@ -69,3 +88,27 @@ ggplot(data_resid%>%filter(species != "River sockeye", year<2012), aes( y = smoo
   theme_sleek() 
 
 ggsave("figs/all-prod-10_avg.jpeg", width = 8, height = 4.6, units = "in")
+
+data_resid_inv <- resid_data_pse_cc %>%
+  pivot_wider(names_from = parameter, values_from = datavalue) %>%
+  drop_na(Recruits) %>%
+  mutate(prod = log(Recruits/Spawners)) %>%
+  drop_na(prod) %>%
+  group_by(location, species, year) %>%
+  summarise(avg_prod = mean(prod)) %>%
+  mutate(smooth_avg_prod = zoo::rollmean(avg_prod, k = 5, fill = NA, align = "right"))%>%
+  mutate(smooth_10_avg_prod = zoo::rollmean(avg_prod, k = 10, fill = NA, align = "right"))%>%
+  select(species, year,avg_prod,smooth_avg_prod,smooth_10_avg_prod)
+
+ggplot() +
+  geom_line(data=data_resid%>%filter(species != "River sockeye", year<2012), aes( y = smooth_10_avg_prod, x = year),size=1.5, color="dark grey")+ 
+  facet_wrap(~species) +
+  ylim(-0.25,1) +
+  theme_sleek() +
+  geom_line(data=data_resid_inv%>%filter(species != "River sockeye", year<2012), 
+            aes(fill=location, y = smooth_10_avg_prod, x = year), 
+            size=0.5, color="light grey") +
+  geom_line(data=data_resid%>%filter(species != "River sockeye", year<2012), aes( y = smooth_10_avg_prod, x = year),size=1.5, color="dark grey")+ 
+  labs(x = "Brood year", y = "Productivity index (10 yr. mean)") 
+
+ggsave("figs/ind-prod-10_avg.jpeg", width = 8, height = 4.6, units = "in")
